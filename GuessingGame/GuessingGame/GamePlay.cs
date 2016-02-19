de061d45d3ca14f086e6ed679d0580348156985d
tmaiorana23 <tmaiorana23@gmail.com>
2016-02-19 13:39:23 -0500

    TMSWG-3 Guessing Game, added Too high, Too low

---
+++ GuessingGame/GuessingGame/GamePlay.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame
+{
+    public class GamePlay
+    {
+        public bool PlayRound(string name)
+        {
+            //create a Random object
+            Random random = new Random();
+            
+            //determine the number to guess
+            int theAnswer = random.Next(1, 21);
+
+            bool isNumberGuessed = false;
+            do
+            {
+
+                // get the player's guess
+                Console.WriteLine("{0}, Please enter your guess: ", name);
+                Console.Write("The number is between 1-20 ");
+                string playerInput = Console.ReadLine();
+
+                //need to make sure we got a number from the user
+                int playerGuess;
+                if (int.TryParse(playerInput, out playerGuess))
+                {
+                    if (theAnswer > playerGuess)
+                    {
+                        Console.WriteLine("{0}, is too low!", playerGuess);
+                    }
+                    else if (theAnswer < playerGuess)
+                    {
+                        Console.WriteLine("{0}, is too high!", playerGuess);
+                    }
+                    else (theAnswer == playerGuess)
+                    {
+                        Console.WriteLine("{0}, YOU GOT IT DUDE!!!", name.ToUpper());
+                        isNumberGuessed = true;
+                    }
+
+                }
+            } while (!isNumberGuessed);
+
+
+
+            //Ask user if they want to play again
+            bool playAnotherRound = false;
+            Console.Write("{0}, would you like to play again? (y/n): ", name);
+            string response = Console.ReadLine();
+
+            if (response.ToUpper() == "Y")
+            {
+                playAnotherRound = true;
+            }
+
+            return playAnotherRound;
+        }
+    }
+}
---
+++ GuessingGame/GuessingGame/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            //Gets the Player's name
+            Console.Write("Please Enter your Name: ");
+            string name = Console.ReadLine();
+
+            bool keepPlaying = false;
+            do
+            {
+                //creates a GamePlay Object
+                GamePlay game = new GamePlay();
+               keepPlaying = game.PlayRound(name);
+            } while (keepPlaying);
+
+        }
+    }
+}
