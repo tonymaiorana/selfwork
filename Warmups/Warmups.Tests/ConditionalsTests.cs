fc8b4002ac228ad0d59237ca93e5cab775b07a5e
tmaiorana23 <tmaiorana23@gmail.com>
2016-02-19 14:22:29 -0500

    Warmups

---
+++ Warmups/Warmups.Tests/ConditionalsTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warmups.Tests
+{
+    [TestFixture]
+   public class ConditionalsTests
+    {
+        [TestCase(true, true, true)]
+        [TestCase(true, false, false)]
+        [TestCase(false, false, true)]
+        public void AreWeInTrouble_Tests(bool aSmile, bool bSmile, bool expectedResult)
+        {
+            Conditionals cond = new Conditionals(); //Creates Object from class Conditionals calling it cond, which creates a new instance of Conditionals
+
+            Assert.AreEqual(expectedResult, cond.AreWeInTrouble(aSmile, bSmile)); //Assert.AreEqual is NUnit method "cond." is method call of our object
+        }
+
+    }
+}
---
+++ Warmups/Warmups/Conditionals.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warmups
+{
+    public class Conditionals
+    {
+        /// <summary>
+        /// Exercise 1 - MischeviousChildren
+        /// </summary>
+        /// <param name="ASmile"></param>
+        /// <param name="bSmile"></param>
+        /// <returns></returns>
+        public bool AreWeInTrouble(bool aSmile, bool bSmile)
+        {
+            if (aSmile == bSmile)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
