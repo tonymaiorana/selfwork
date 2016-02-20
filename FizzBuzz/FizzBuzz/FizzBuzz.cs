cbc300da41df5ded1063d88db4dbad019d0adf3c
tmaiorana23 <tmaiorana23@gmail.com>
2016-02-20 15:35:05 -0500

    TMSWG-5 FizzBuzz completed

---
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+   public class FizzBuzz
+    {
+        public void Run()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.Write("FizzBuzz: ");
+                    
+                }
+
+                else if (i % 3 == 0)
+                {
+                    Console.Write("Fizz: ");
+                }
+
+                else if (i % 5 == 0)
+                {
+                    Console.Write("Buzz: ");
+                }
+                Console.WriteLine(i);
+            }
+
+
+        }
+
+
+    }
+}
