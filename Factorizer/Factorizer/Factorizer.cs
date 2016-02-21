ca8d8f39d0835d92204c22ce4162486165a8dc66
tmaiorana23 <tmaiorana23@gmail.com>
2016-02-21 13:44:37 -0500

    TMSWG-6 Factorizer started, just basic setup for now

---
+++ Factorizer/Factorizer/Factorizer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorizer
+{
+    public class Factorizer
+    {
+        public void Run ()
+        {
+            
+            Console.Write("Please input a number to get it's factors: ");
+
+            string input = Console.ReadLine();
+
+            int num;
+
+            if (int.TryParse(input, out num))
+            {
+                Console.WriteLine("The factors of " + num + " are: ");
+            }
+
+            else
+            {
+                Console.Write("Please make sure you enter a valid number");
+            }
+
+        }
+    }
+}
---
+++ Factorizer/Factorizer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorizer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Factorizer!");
+
+            //creates Factorizer object
+            Factorizer cond = new Factorizer();
+            cond.Run();
+
+            Console.ReadLine();
+        }
+    }
+}
