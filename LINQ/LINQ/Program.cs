bcfcd5a93b6a7f060c0ec331855896df176042c8
tmaiorana23 <tmaiorana23@gmail.com>
2016-02-29 13:01:32 -0500

    TMSWG-7 adding sample file to work on later

---
+++ LINQ/LINQ/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ
+{
+    class Program
+    {
+        /* Practice your LINQ!
+         * You can use the methods in Data Loader to load products, customers, and some sample numbers
+         * 
+         * NumbersA, NumbersB, and NumbersC contain some ints
+         * 
+         * The product data is flat, with just product information
+         * 
+         * The customer data is hierarchical as customers have zero to many orders
+         */
+        static void Main()
+        {
+            //PrintOutOfStock();
+            InStockMoreThan3();
+
+            Console.ReadLine();
+        }
+
+        private static void PrintOutOfStock()
+        {
+            var products = DataLoader.LoadProducts();
+
+            //var results = products.Where(p => p.UnitsInStock == 0);
+            var results = from p in products
+                where p.UnitsInStock == 0
+                select p;
+
+            foreach (var product in results)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+
+        //2. Find all products that are in stock and cost more than 3.00 per unit.
+        private static void InStockMoreThan3()
+        {
+            var products = DataLoader.LoadProducts();
+
+            //var results = products.Where(p => p.UnitsInStock > 0 && p.UnitPrice > 3);
+            var results = from p in products
+                where p.UnitsInStock > 0 && p.UnitPrice > 3
+                select p;
+
+            foreach (var product in results)
+            {
+                Console.WriteLine("{0} has {1} in stock with unit price {2}", product.ProductName,
+                    product.UnitsInStock, product.UnitPrice);
+            }
+        }
+    }
+}
