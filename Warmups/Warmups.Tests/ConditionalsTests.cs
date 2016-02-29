a97373db38ff559ea3285ca9a88746358c0d78e1
tmaiorana23 <tmaiorana23@gmail.com>
2016-02-28 22:13:27 -0500

    TMSWG-4 Completed 25 warmups, working on last 2

--- Warmups/Warmups.Tests/ConditionalsTests.cs
+++ Warmups/Warmups.Tests/ConditionalsTests.cs
@@ -5,6 +5,7 @@ using System.Configuration;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -112,5 +113,178 @@ namespace Warmups.Tests
             Assert.AreEqual(expectedResult, actual);
 
         }
+
+        [TestCase("kitten", 1, "ktten")]
+        [TestCase("kitten", 0, "itten")]
+        [TestCase("kitten", 4, "kittn")]
+        public void MissingChar_Test(string str, int n, string expectedResult)
+        {
+            Conditionals cond = new Conditionals();
+            string actual = cond.MissingChar(str, n);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase("code", "eodc")]
+        [TestCase("a", "a")]
+        [TestCase("ab", "ba")]
+        public void FrontBack_Test(string str, string expectedResult)
+        {
+            Conditionals cond = new Conditionals();
+            string actual = cond.FrontBack(str);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase("Microsoft", "MicMicMic")]
+        [TestCase("Chocolate", "ChoChoCho")]
+        [TestCase("at", "atatat")]
+        public void Front3_Test(string str, string expectedResult)
+        {
+            Conditionals cond = new Conditionals();
+            string actual = cond.Front3(str);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase("cat", "tcatt")]
+        [TestCase("Hello", "oHelloo")]
+        [TestCase("a", "aaa")]
+        public void BackAround_Test(string str, string expectedResult)
+        {
+            Conditionals cond = new Conditionals();
+            string actual = cond.BackAround(str);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase(3, true)]
+        [TestCase(10, true)]
+        [TestCase(8, false)]
+        public void Multiple3or5_Test(int number, bool expectedResult)
+        {
+            Conditionals cond = new Conditionals();
+            bool actual = cond.Multiple3or5(number);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase("hi there", true)]
+        [TestCase("hi", true)]
+        [TestCase("high up", false)]
+        public void StartHi_Test(string str, bool expectedResult)
+        {
+            Conditionals cond = new Conditionals();
+            bool actual = cond.StartHi(str);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase(120, -1, true)]
+        [TestCase(-1, 120, true)]
+        [TestCase(2, 120, false)]
+        public void IcyHot_Test(int temp1, int temp2, bool expectedResult)
+        
+        {
+            Conditionals cond = new Conditionals();
+            bool actual = cond.IcyHot(temp1, temp2);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase(12, 99, true)]
+        [TestCase(21, 12, true)]
+        [TestCase(8, 99, false)]
+        public void Between10and20_Test(int a, int b, bool expectedResult)
+        {
+            Conditionals cond = new Conditionals();
+            bool actual = cond.Between10and20(a, b);
+            Assert.AreEqual(expectedResult, actual);
+
+        }
+
+        [TestCase(13, 20, 10, true)]
+        [TestCase(20, 19, 10, true)]
+        [TestCase(20, 10, 12, false)]
+        public void HasTeen_Test(int a, int b, int c, bool expectedResult)
+        {
+            Conditionals cond = new Conditionals();
+            bool actual = cond.HasTeen(a, b, c);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase(13, 99, true)]
+        [TestCase(21, 19, true)]
+        [TestCase(13, 13, false)]
+        public void SoAlone_Test(int a, int b, bool expectedResult)
+        {
+            Conditionals cond = new Conditionals();
+            bool actual = cond.SoAlone(a, b);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase("adelbc", "abc")]
+        [TestCase("adelHello", "aHello")]
+        [TestCase("adedbc", "adedbc")]
+        public void RemoveDel_Test(string str, string expectedResult)
+        {
+            Conditionals cond = new Conditionals();
+            string actual = cond.RemoveDel(str);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase("mix snacks", true)]
+        [TestCase("pix snacks", true)]
+        [TestCase("piz snacks", false)]
+        public void IxStart_Test(string str, bool expectedResult)
+        {
+            Conditionals cond = new Conditionals();
+            bool actual = cond.IxStart(str);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase("ozymandias", "oz")]
+        [TestCase("bzoo", "z")]
+        [TestCase("oxx", "o")]
+        public void StartOz_Test(string str, string expectedResult)
+        {
+            Conditionals cond = new Conditionals();
+            string actual = cond.StartOz(str);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase(1, 2, 3, 3)]
+        [TestCase(1, 3, 2, 3)]
+        [TestCase(3, 2, 1, 3)]
+        public void Max_Test(int a, int b, int c, int expectedResult)
+        {
+            Conditionals cond = new Conditionals();
+            int actual = cond.Max(a, b, c);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase(8, 13, 8)]
+        [TestCase(13, 8, 8)]
+        [TestCase(13, 7, 0)]
+        public void Closer_Test(int a, int b, int expectedResult)
+        {
+            Conditionals cond = new Conditionals();
+            int actual = cond.Closer(a, b);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        //[TestCase("Hello", true)]
+        //[TestCase("Heelle", true)]
+        //[TestCase("Heelele", false)]
+        //public void GotE_Test(string str, bool expectedResult)
+        //{
+        //    Conditionals cond = new Conditionals();
+        //    bool actual = cond.GotE(str);
+        //    Assert.AreEqual(expectedResult, actual);
+        //}
+
+        [TestCase("Hello", "HeLLO")]
+        [TestCase("hi there", "hi thERE")]
+        [TestCase("hi", "HI")]
+        public void EndUp_Tests(string str, string expectedResult)
+        {
+            Conditionals cond = new Conditionals();
+            string actual = cond.EndUp(str);
+            Assert.AreEqual(expectedResult, actual);
+        }
+        
     }
 }
--- Warmups/Warmups/Conditionals.cs
+++ Warmups/Warmups/Conditionals.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -43,6 +46,7 @@ namespace Warmups
 
             return true;
         }
+
         /// <summary>
         /// Exercise 3 SumDouble
         /// </summary>
@@ -59,6 +63,7 @@ namespace Warmups
   
                 return a + b;
         }
+
         /// <summary>
         /// Exercise 4 Diff21
         /// </summary>
@@ -73,6 +78,7 @@ namespace Warmups
 
             return Math.Abs(n - 21)*2;
         }
+
         /// <summary>
         /// Exercise 5 ParrotTrouble
         /// </summary>
@@ -104,6 +110,7 @@ namespace Warmups
 
             return false;
         }
+
         /// <summary>
         /// Exercise 07 NearHundred
         /// </summary>
@@ -118,6 +125,7 @@ namespace Warmups
 
             return false;
         }
+
         /// <summary>
         /// Exercise 08 PosNeg
         /// </summary>
@@ -139,6 +147,7 @@ namespace Warmups
 
             return false;
         }
+
         /// <summary>
         /// Exercise 09 NotString
         /// </summary>
@@ -156,5 +165,314 @@ namespace Warmups
                 return  "not "+ s;
             }
         }
+        /// <summary>
+        /// Exercise 10 MissingChar
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public string MissingChar(string str, int n)
+        {
+            if (str != "")
+            {
+                return str.Remove(n, 1);
+            }
+            else
+                return str;
+
+        }
+
+        /// <summary>
+        /// Exercise 11 FrontBack
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public string FrontBack(string str)
+        {
+            if (str != "")
+            {
+                char[] a = str.ToArray();
+                char test = a[0];
+                a[0] = a[a.Length - 1];
+                a[a.Length - 1] = test;
+                string b = new string(a);
+                return b;
+
+            }
+            else
+            {
+                return str;
+            }
+        }
+        /// <summary>
+        /// Exercise 12 Front3
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public string Front3(string str)
+        {
+            if (str.Length < 3)
+            {
+                return str + str + str;
+            }
+            else if (str.Length > 3)
+            {
+                str = str.Substring(0, 3);
+                return str + str + str;
+            }
+            else
+            {
+                return str;
+            }
+        }
+
+        /// <summary>
+        /// Exercise 13 BackAround
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public string BackAround(string str)
+        {
+            if (str.Length >= 1)
+            {
+                char[] c = str.ToArray();
+                c[0] = c[c.Length - 1];
+                string aNewString = c[0].ToString() + str + c[0].ToString();
+                return aNewString;
+            }
+            else
+                return str;
+        }
+
+        /// <summary>
+        /// Exercise 14 Multiple3or5
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public bool Multiple3or5(int number)
+        {
+            if((number > 0) && (number % 3==0) || (number % 5 == 0))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Exercise 15 StartHi
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public bool StartHi(string str)
+        {
+            if ( str.Length > 2 && str.StartsWith("hi "))
+            {
+                return true;
+            }
+            else if (str.Length <= 2 && str.StartsWith("hi"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Exercise 16 IcyHot
+        /// </summary>
+        /// <param name="temp1"></param>
+        /// <param name="temp2"></param>
+        /// <returns></returns>
+        public bool IcyHot(int temp1, int temp2)
+        {
+            if((temp1 >= 100) || (temp2 >= 100) && (temp1 <= 0 || temp2 <=0))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Exercise 17 Between10and20
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public bool Between10and20(int a, int b)
+        {
+            if(((a <= 20) && (a >= 10)) || ((b <=20) && (b >= 10)))
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+        /// <summary>
+        /// Exercise 18 HasTeen
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public bool HasTeen(int a, int b, int c)
+        {
+            if((a <= 19) && (a >= 13) ||(b <= 19) && (b >= 13) ||(c <= 19 && (c >= 13)))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool SoAlone(int a, int b)
+        {
+            if ((a <= 19) && (a >= 13) && (b <= 19) && (b >= 13))
+            {
+                return false;
+            }
+
+            else if ((a <= 19) && (a >= 13) || (b <= 19) && (b >= 13))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Exercise 20 RemoveDel
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public string RemoveDel(string str)
+        {
+            if (str.Contains("del"))
+            {
+                string newString = str.Remove(1, 3);
+                return newString;
+            }
+            return str;
+        }
+
+        /// <summary>
+        /// Exercise 21 IxStart
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public bool IxStart(string str)
+        {
+            if (str.Substring(1, 2).Contains("ix"))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Exercise 22 StartOz
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public string StartOz(string str)
+        {
+            if(str.StartsWith("oz"))
+            {
+                string newString = str.Substring(0, 2);
+                return newString;
+            }
+
+            else if (str.Substring(1, 1).Contains("z"))
+            {
+                string newString = "z";
+                return newString;
+            }
+
+           else if (str.StartsWith("o"))
+            {
+                string newString = "o";
+                return newString;
+            }
+
+            return str;
+        }
+
+        /// <summary>
+        /// Exercise 23 Max
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public int Max(int a, int b, int c)
+        {
+           int max = 0;
+            if ((a > b) && (a > c))
+            {
+                max = a;
+            }
+
+            else if ((b > a) && (b > c))
+            {
+                max = b;
+            }
+
+            else if ((c > a) && (c > b))
+            {
+                max = c;
+            }
+            return max;
+        }
+
+        /// <summary>
+        /// Exercise 24 Closer
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int Closer(int a, int b)
+        {
+            if (Math.Abs(10 - a) == Math.Abs(10 - b))
+            {
+                return 0;
+            }
+                if ((a - 10) < (b - 10))
+            {
+                return a;
+            }
+
+            return b;
+        }
+
+        /// <summary>
+        /// Exercise 25 GotE
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        //public bool GotE(string str)
+        //{
+        //    if (str.Contains("e"))
+        //    {
+        //        int i = 0;
+        //       foreach(st "e"; i++)
+        //        {
+
+        //        }
+        //    }
+        //}
+
+        
+        public string EndUp(string str)
+        {
+            if (str.Length > 3)
+            {
+                return str.Substring(2).ToUpper();
+            }
+            return str.ToUpper();
+        }
+
     }
 }
