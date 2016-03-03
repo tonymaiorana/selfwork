6ace012337aea35637992ccdd650d7e046be9e41
tmaiorana23 <tmaiorana23@gmail.com>
2016-03-03 17:32:11 -0500

    TMSWG-4 starting Loops

---
+++ Warmups/Warmups.Tests/LoopTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Warmups.Tests
+{
+    [TestFixture]
+    public class LoopTests
+    {
+        [TestCase("Hi", 2, "HiHi")]
+        [TestCase("Hi", 3, "HiHiHi")]
+        [TestCase("Hi", 1, "Hi")]
+        public void StringTimes_Test(string str, int n, string expectedResult)
+        {
+            Loops trial = new Loops();
+            string actual = trial.StringTimes(str, n);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase("Chocolate", 2, "ChoCho")]
+        [TestCase("Chocolate", 3, "ChoChoCho")]
+        [TestCase("Abc", 3, "AbcAbcAbc")]
+        public void FrontTimes_Test(string str, int n, string expectedResult)
+        {
+            Loops trial = new Loops();
+            string actual = trial.FrontTimes(str, n);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase("abcxx", 1)]
+        [TestCase("xxx", 2)]
+        [TestCase("xxxx", 3)]
+        public void CountXX_Test(string str, int expectedResult)
+        {
+            Loops trial = new Loops();
+            int actual = trial.CountXX(str);
+            Assert.AreEqual(expectedResult, actual);
+        }
+    }
+}
---
+++ Warmups/Warmups/Loops.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warmups
+{
+   public class Loops
+    {
+        /// <summary>
+        /// Exercise 01 StringTimes
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+       public string StringTimes(string str, int n)
+        {
+            string newstring = "";
+            for (int i = 0; i < n; i++)
+            {
+                newstring += str;
+            }
+            return newstring;
+        }
+
+       public string FrontTimes(string str, int n)
+       {
+           string newstring = "";
+           for (int i = 0; i < n; i++)
+           {
+               newstring += str.Substring(0, 3);
+           }
+           return newstring;
+       }
+
+        /// <summary>
+        /// Exercise 03 CountXX
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+       public int CountXX(string str)
+       {
+           
+       }
+    }
+}
