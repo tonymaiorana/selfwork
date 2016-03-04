0b6e6a2284aca29111aea10ddd7a5cce9f8fd014
tmaiorana23 <tmaiorana23@gmail.com>
2016-03-04 17:01:53 -0500

    TMSWG-2 adding Calculator Kata from class

---
+++ StringCalculatorKata2/StringCalculatorKata.Tests/KataMathMakerTest.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using StringCalculatorKata2;
+
+namespace StringCalculatorKata.Tests
+{
+    [TestFixture]
+   public class KataMathMakerTest
+    {
+        private KataMathMaker _calc;
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            _calc = new KataMathMaker();
+        }
+
+
+        [Test]
+        public void Add_EmptyString_ReturnZero()
+        {
+            Assert.AreEqual(0, _calc.Add(""));
+        }
+
+        [TestCase("1", 1)]
+        [TestCase("17", 17)]
+        public void Add_SingleNumber_ReturnNumber(string number, int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, _calc.Add(number));
+        }
+
+        [TestCase("1,2", 3)]
+        [TestCase("4,5", 9)]
+        public void Add_TwoNumbers_ReturnSum(string numbers, int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, _calc.Add(numbers));
+        }
+
+        [TestCase("1,2,3", 6)]
+        [TestCase("9,8,7,6,5", 35)]
+        public void Add_UnknownNumbers_ReturnSum(string numbers, int expectedResult)
+        {
+            
+            Assert.AreEqual(expectedResult, _calc.Add(numbers));
+        }
+
+        [TestCase("1,2\n3", 6)]
+        [TestCase("9\n8,7\n6,5", 35)]
+        public void Add_NewLineSeparator_ReturnSum(string numbers, int expectedResult)
+        {
+
+            Assert.AreEqual(expectedResult, _calc.Add(numbers));
+        }
+
+        [TestCase("//;\n1;2", 3)]
+        [TestCase("//|\n9|8|7|6|5", 35)]
+        public void Add_SpecifySeparator_ReturnSum(string numbers, int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, _calc.Add(numbers));
+        }
+
+        [TestCase("-1,2", -1)]
+        [TestCase("1,2,-3", -1)]
+        public void Add_NegativeNumbers_ReturnNeg(string numbers, int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, _calc.Add(numbers));
+        }
+
+        [TestCase("1000, 2", 2)]
+        [TestCase("12, 13, 2002", 25)]
+        public void Add_IgnoreThousands_ReturnSum(string numbers, int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, _calc.Add(numbers));
+        }
+
+        [TestCase("//[***],\n1***2***3", 6)]
+        [TestCase("//[|||||]\n9|||||8|||||7|||||6|||||5", 35)]
+        public void Add_AnyLengthDelim_ReturnSum(string numbers, int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, _calc.Add(numbers));
+        }
+
+        [TestCase("//[*][%][\n1*2%3", 6)]
+        [TestCase("//[|][*][@]\n1@2*7|9", 19)]
+        public void Add_MultipleDelim_ReturnSum(string numbers, int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, _calc.Add(numbers));
+        }
+    }
+
+}
---
+++ StringCalculatorKata2/StringCalculatorKata2/KataMathMaker.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorKata2
+{
+    public class KataMathMaker
+    {
+        public int Add(string numbers)
+        {
+            int sum = 0;
+
+            if (String.IsNullOrEmpty(numbers))
+            {
+                return sum;
+            }
+
+            string[] del;
+            if (numbers.StartsWith("//"))
+            {
+                string[] parts = numbers.Split('\n');
+
+                if (parts[0].Length == 3)
+                {
+                    del = new string[] {parts[0].Remove(0,2)};
+                }
+
+                else
+                {
+                    List<string> delims = new List<string>();
+                    foreach (char c in parts[0])
+                    {
+                        if (c != '/' && c != '[' && c != ']')
+                        {
+                            delims.Add(c.ToString());
+                        }
+                    }
+                    //del = new string[] {parts[0].Substring(3,
+                    //parts[0].Length - 4)
+                    //};
+                }
+
+                numbers = parts[1];
+            }
+
+            else
+            {
+                del = new string[] {",", "\n"};
+            }
+
+            string[] arrNums = numbers.Split(del, StringSplitOptions.RemoveEmptyEntries);
+
+            if (arrNums.Length == 1)
+            {
+                sum = int.Parse(numbers);
+            }
+            else
+            {
+                int number = 0;
+                bool isNegativeFound = false;
+                for (int i = 0; i < arrNums.Length; i++)
+                {
+                    number = int.Parse(arrNums[i]);
+                    if (number > 0 && !isNegativeFound)
+                    {
+                        if (number < 1000)
+                        {
+                            sum += number;
+                        }
+                    }
+                    else
+                    {
+                        if (!isNegativeFound)
+                        {
+                            Console.Write("Negatives not allowed: {0}", number);
+                        }
+                        else
+                        {
+                            Console.Write(",{0}", number);
+                        }
+
+                        isNegativeFound = true;
+                        sum = -1;
+                    }
+
+                }
+            }
+
+            return sum;
+
+        }
+    }
+}
