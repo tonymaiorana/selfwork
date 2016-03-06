0ff7c19a6f603541f00ee9b1d41c3d91d23d1d02
tmaiorana23 <tmaiorana23@gmail.com>
2016-03-06 16:42:14 -0500

    TMSWG-4 Started and completed Logic warmups

---
+++ Warmups/Warmups.Tests/LogicTests.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace Warmups.Tests
+{
+    [TestFixture]
+    class LogicTests
+    {
+        [TestCase(30, false, false)]
+        [TestCase(50, false, true)]
+        [TestCase(70, true, true)]
+        public void GreatParty_Test(int cigars, bool IsWeekend, bool expectedResult)
+        {
+            Logic test = new Logic();
+            bool actual = test.GreatParty(cigars, IsWeekend);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase(5, 10, 2)]
+        [TestCase(5, 2, 0)]
+        [TestCase(5, 5, 1)]
+        public void CanHazTable_Test(int yourStyle, int dateStyle, int expectedResult)
+        {
+            Logic test = new Logic();
+            int actual = test.CanHazTable(yourStyle, dateStyle);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase(70, false, true)]
+        [TestCase(95, false, false)]
+        [TestCase(95, true, true)]
+        public void PlayOutside_Test(int temp, bool isSummer, bool expectedResult)
+        {
+            Logic test = new Logic();
+            bool actual = test.PlayOutside(temp, isSummer);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase(60, false, 0)]
+        [TestCase(65, false, 1)]
+        [TestCase(65, true, 0)]
+        public void CaughtSpeeding_Test(int speed, bool isBirthday, int expectedResult)
+        {
+            Logic test = new Logic();
+            int actual = test.CaughtSpeeding(speed, isBirthday);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase(3, 4, 7)]
+        [TestCase(9, 4, 20)]
+        [TestCase(10, 11, 21)]
+        public void SkipSum_Test(int a, int b, int expectedResult)
+        {
+            Logic test = new Logic();
+            int actual = test.SkipSum(a, b);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase(1, false, "7:00")]
+        [TestCase(5, false, "7:00")]
+        [TestCase(0, false, "10:00")]
+        [TestCase(0, true, "off")]
+        public void AlarmClock_Test(int day, bool vacation, string expectedResult)
+        {
+            Logic test = new Logic();
+            string actual = test.AlarmClock(day, vacation);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase(6, 4, true)]
+        [TestCase(4, 5, false)]
+        [TestCase(1, 5, true)]
+        public void LoveSix_Test(int a, int b, bool expectedResult)
+        {
+            Logic test = new Logic();
+            bool actual = test.LoveSix(a, b);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase(5, false, true)]
+        [TestCase(11, false, false)]
+        [TestCase(11, true, true)]
+        public void InRange_Test(int n, bool outsideMore, bool expectedResult)
+        {
+            Logic test = new Logic();
+            bool actual = test.InRange(n, outsideMore);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase(22, true)]
+        [TestCase(23, true)]
+        [TestCase(24, false)]
+        public void SpecialEleven_Test(int n, bool expectedResult)
+        {
+            Logic test = new Logic();
+            bool actual = test.SpecialEleven(n);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase(20, false)]
+        [TestCase(21, true)]
+        [TestCase(22, true)]
+        public void Mod20_Test(int n, bool expectedResult)
+        {
+            Logic test = new Logic();
+            bool actual = test.Mod20(n);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase(3, true)]
+        [TestCase(10, true)]
+        [TestCase(15, false)]
+        public void Mod35_Test(int n, bool expectedResult)
+        {
+            Logic test = new Logic();
+            bool actual = test.Mod35(n);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase(false, false, false, true)]
+        [TestCase(false, false, true, false)]
+        [TestCase(true, false, false, false)]
+        public void AnswerCell_Test(bool isMorning, bool isMom, bool isAsleep, bool expectedResult)
+        {
+            Logic test = new Logic();
+            bool actual = test.AnswerCell(isMorning, isMom, isAsleep);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase(1, 2, 3, true)]
+        [TestCase(3, 1, 2, true)]
+        [TestCase(3, 2, 2, false)]
+        public void TwoIsOne_Test(int a, int b, int c, bool expectedResult)
+        {
+            Logic test = new Logic();
+            bool actual = test.TwoIsOne(a, b, c);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase(1, 2, 4, false, true)]
+        [TestCase(1, 2, 1, false, false)]
+        [TestCase(1, 1, 2, true, true)]
+        public void AreInOrder_Test(int a, int b, int c, bool bOk, bool expectedResult)
+        {
+            Logic test = new Logic();
+            bool actual = test.AreInOrder(a, b, c, bOk);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase(2, 5, 11, false, true)]
+        [TestCase(5, 7, 6, false, false)]
+        [TestCase(5, 5, 7, true, true)]
+        public void InOrderEqual_Test(int a, int b, int c, bool equalOK, bool expectedResult)
+        {
+            Logic test = new Logic();
+            bool actual = test.InOrderEqual(a, b, c, equalOK);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase(23, 19, 13, true)]
+        [TestCase(23, 19, 13, true)]
+        [TestCase(23, 19, 13, true)]
+        public void LastDigit_Test(int a, int b, int c, bool expectedResult)
+        {
+            Logic test = new Logic();
+            bool actual = test.LastDigit(a,b,c);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase(2, 3, true, 5)]
+        [TestCase(3, 3, true, 7)]
+        [TestCase(3, 3, false, 6)]
+        [TestCase(4,4,true,9)]
+        public void RollDice_Test(int die1, int die2, bool noDoubles, int expectedResult)
+        {
+            Logic test = new Logic();
+            int actual = test.RollDice(die1, die2, noDoubles);
+            Assert.AreEqual(expectedResult, actual);
+        }
+    }
+}
---
+++ Warmups/Warmups/Logic.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warmups
+{
+    public class Logic
+    {
+
+        /// <summary>
+        /// Exercise 01 GreatParty
+        /// </summary>
+        /// <param name="cigars"></param>
+        /// <param name="isWeekend"></param>
+        /// <returns></returns>
+        public bool GreatParty(int cigars, bool isWeekend)
+        {
+            if ((cigars >= 40) && (cigars <= 60) && !isWeekend)
+            {
+                return true;
+            }
+
+            else if ((cigars >= 40) && isWeekend)
+            {
+                return true;
+
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Exercise 02 CanHazTable
+        /// </summary>
+        /// <param name="yourStyle"></param>
+        /// <param name="dateStyle"></param>
+        /// <returns></returns>
+        public int CanHazTable(int yourStyle, int dateStyle)
+        {
+            int no = 0;
+            int maybe = 1;
+            int yes = 2;
+            if ((yourStyle >= 8 || dateStyle >= 8) &&(yourStyle > 2 && dateStyle > 2))
+            {
+                return yes;
+            }
+
+            if (yourStyle <= 2 || dateStyle <= 2)
+            {
+                return no;
+            }
+            return maybe;
+        }
+
+        /// <summary>
+        /// Exercise 03 PlayOutside
+        /// </summary>
+        /// <param name="temp"></param>
+        /// <param name="isSummer"></param>
+        /// <returns></returns>
+        public bool PlayOutside(int temp, bool isSummer)
+        {
+            if (temp <= 100 && temp >=60 && isSummer)
+            {
+                return true;
+            }
+            if (temp <= 90 && temp >= 60 && !isSummer)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Exercise 04 CaughtSpeeding
+        /// </summary>
+        /// <param name="speed"></param>
+        /// <param name="isBirthday"></param>
+        /// <returns></returns>
+        public int CaughtSpeeding(int speed, bool isBirthday)
+        {
+            int noTicket = 0;
+            int smallTicket = 1;
+            int bigTicket = 2;
+
+            if (speed > 60 && speed <= 80 && (!isBirthday))
+            {
+                return smallTicket;
+            }
+            if (speed > 80 && !isBirthday)
+            {
+                return bigTicket;
+            }
+            if ((speed >= 65) || (speed <= 85) && isBirthday)
+            {
+                return noTicket;
+            }
+            return noTicket;
+        }
+
+        /// <summary>
+        /// Exercise 05 SkipSum
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int SkipSum(int a, int b)
+        {
+            if ((a + b >= 10) && (a + b <= 19))
+            {
+                return 20;
+            }
+            return a + b;
+        }
+
+        /// <summary>
+        /// Exercise 06 AlarmClock
+        /// </summary>
+        /// <param name="day"></param>
+        /// <param name="vacation"></param>
+        /// <returns></returns>
+        public string AlarmClock(int day, bool vacation)
+        {
+            string weekDayAlarm = "7:00";
+            string weekendAlarm = "10:00";
+            string vacationAlarm = "off";
+
+            if (vacation)
+            {
+                return vacationAlarm;
+            }
+
+            if (day == 0 || day == 6 && !vacation)
+            {
+                return weekendAlarm;
+            }
+            return weekDayAlarm;
+        }
+
+        /// <summary>
+        /// Exercise 07 LoveSix
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public bool LoveSix(int a, int b)
+        {
+            if (a == 6 || b == 6)
+            {
+                return true;
+            }
+            if ((a + b == 6) || (a - b == 6))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Exercise 08 InRange
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="outsideMore"></param>
+        /// <returns></returns>
+        public bool InRange(int n, bool outsideMore)
+        {
+            if (n >= 1 && n <= 10 && !outsideMore)
+            {
+                return true;
+            }
+            if (n <= 1 || n >= 10 && outsideMore)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Exercise 09 SpecialEleven
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public bool SpecialEleven(int n)
+        {
+            if (n % 11 == 0 || n % 11 == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Exercise 10 Mod20
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public bool Mod20(int n)
+        {
+            if (n % 20 == 1 || n % 20 == 2)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Exercise 11 Mod35
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public bool Mod35(int n)
+        {
+            if (n % 3 == 0 && n % 5 == 0)
+            {
+                return false;
+            }
+            if (n % 3 == 0 || n % 5 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Exercise 12 AnswerCell
+        /// </summary>
+        /// <param name="isMorning"></param>
+        /// <param name="isMom"></param>
+        /// <param name="isAsleep"></param>
+        /// <returns></returns>
+        public bool AnswerCell(bool isMorning, bool isMom, bool isAsleep)
+        {
+            if (isAsleep)
+            {
+                return false;
+            }
+            if (isMorning && !isMom)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Exercise 13 TwoIsOne
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public bool TwoIsOne(int a, int b, int c)
+        {
+            if ((a + b == c) || (b + c == a) || (a + c == b))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Exercise 14 AreInOrder
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <param name="bOk"></param>
+        /// <returns></returns>
+        public bool AreInOrder(int a, int b, int c, bool bOk)
+        {
+            if ((b > a) && (c > b) && !bOk)
+            {
+                return true;
+            }
+            if (c > b && bOk)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Exercise 15 InOrderEqual
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <param name="equalOk"></param>
+        /// <returns></returns>
+        public bool InOrderEqual(int a, int b, int c, bool equalOk)
+        {
+            if (a < b && b < c && !equalOk)
+            {
+                return true;
+            }
+            if (a <= b && b <= c && equalOk)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Exercise 16 LastDigit
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public bool LastDigit(int a, int b, int c)
+        {
+            if ((a % 10 == b % 10) || (a % 10 == c % 10))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Exercise 17 RollDice
+        /// </summary>
+        /// <param name="die1"></param>
+        /// <param name="die2"></param>
+        /// <param name="noDoubles"></param>
+        /// <returns></returns>
+        public int RollDice(int die1, int die2, bool noDoubles)
+        {
+            if (noDoubles && die1 == die2)
+            {
+                return die1 + die2 + 1;
+            }
+            return die1 + die2;
+        }
+    }
+}
