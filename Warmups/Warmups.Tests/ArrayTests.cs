5cb37b0cec07b91bda03f5698a167949b14bf37e
tmaiorana23 <tmaiorana23@gmail.com>
2016-03-09 20:41:04 -0500

    TMSWG-4 finished arrays except for MakePi...getting help from Sarah

--- Warmups/Warmups.Tests/ArrayTests.cs
+++ Warmups/Warmups.Tests/ArrayTests.cs
@@ -27,7 +27,7 @@ namespace Warmups.Tests
         {
             Arrays test = new Arrays();
             bool actual = test.SameFirstLast(numbers);
-            Assert.AreEqual(expectedResult,actual);
+            Assert.AreEqual(expectedResult, actual);
         }
 
         //[TestCase(3, new[] {3, 1, 4})]
@@ -40,9 +40,9 @@ namespace Warmups.Tests
         //    Assert.AreEqual(expectedResult, actual);
         //}
 
-        [TestCase(new[] {1, 2, 3}, new[] {7, 3}, true)]
-        [TestCase(new[] {1, 2, 3}, new[] {7, 3, 2}, false)]
-        [TestCase(new[] {1, 2, 3}, new[] {1, 3}, true)]
+        [TestCase(new[] { 1, 2, 3 }, new[] { 7, 3 }, true)]
+        [TestCase(new[] { 1, 2, 3 }, new[] { 7, 3, 2 }, false)]
+        [TestCase(new[] { 1, 2, 3 }, new[] { 1, 3 }, true)]
         public void CommonEnd_Test(int[] a, int[] b, bool expectedResult)
         {
             Arrays test = new Arrays();
@@ -50,14 +50,116 @@ namespace Warmups.Tests
             Assert.AreEqual(expectedResult, actual);
         }
 
-        //[TestCase(new[] {1, 2, 3}, 6)]
-        //[TestCase(new[] {5, 11, 2}, 18)]
-        //[TestCase(new[] {7, 0, 0}, 7)]
-        //public void Sum(int[] numbers, int expectedResult)
-        //{
-        //    Arrays test = new Arrays();
-        //    int actual = test.Sum(numbers);
-        //    Assert.AreEqual(expectedResult, actual);
-        //}
+        [TestCase(new[] { 1, 2, 3 }, 6)]
+        [TestCase(new[] { 5, 11, 2 }, 18)]
+        [TestCase(new[] { 7, 0, 0 }, 7)]
+        public void Sum(int[] numbers, int expectedResult)
+        {
+            Arrays test = new Arrays();
+            int actual = test.Sum(numbers);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase(new[] { 1, 2, 3 }, new[] { 2, 3, 1 })]
+        [TestCase(new[] { 5, 11, 9 }, new[] { 11, 9, 5 })]
+        [TestCase(new[] { 7, 0, 0 }, new[] { 0, 0, 7 })]
+        public void RotateLeft(int[] numbers, int[] expectedResult)
+        {
+            Arrays test = new Arrays();
+            int[] actual = test.RotateLeft(numbers);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase(new[] { 1, 2, 3 }, new[] { 3, 2, 1 })]
+        [TestCase(new[] { 2, 4, 3 }, new[] { 3, 4, 2 })]
+        [TestCase(new[] { 7, 8, 9 }, new[] { 9, 8, 7 })]
+        public void Reverse_Test(int[] numbers, int[] expectedResult)
+        {
+            Arrays test = new Arrays();
+            int[] actual = test.Reverse(numbers);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase(new[] { 1, 2, 3 }, new[] { 3, 3, 3 })]
+        [TestCase(new[] { 11, 5, 9 }, new[] { 11, 11, 11 })]
+        [TestCase(new[] { 2, 11, 3 }, new[] { 3, 3, 3 })]
+        public void HigherWins_Test(int[] numbers, int[] expectedResult)
+        {
+            Arrays test = new Arrays();
+            int[] actual = test.HigherWins(numbers);
+            Assert.AreEqual(expectedResult, actual);
+
+        }
+
+        [TestCase(new[] { 1, 2, 3 }, new[] { 4, 5, 6 }, new[] { 2, 5 })]
+        [TestCase(new[] { 7, 7, 7 }, new[] { 3, 8, 0 }, new[] { 7, 8 })]
+        [TestCase(new[] { 5, 2, 9 }, new[] { 1, 4, 5 }, new[] { 2, 4 })]
+        public void GetMiddle_Test(int[] a, int[] b, int[] expectedResult)
+        {
+            Arrays test = new Arrays();
+            int[] actual = test.GetMiddle(a, b);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase(new[] { 2, 5 }, true)]
+        [TestCase(new[] { 4, 3 }, true)]
+        [TestCase(new[] { 7, 5 }, false)]
+        public void HasEven_Test(int[] numbers, bool expectedResult)
+        {
+            Arrays test = new Arrays();
+            bool actual = test.HasEven(numbers);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase(new[] { 4, 5, 6 }, new[] { 0, 0, 0, 0, 0, 6 })]
+        [TestCase(new[] { 1, 2 }, new[] { 0, 0, 0, 2 })]
+        [TestCase(new[] { 3 }, new[] { 0, 3 })]
+        public void KeepLast_Test(int[] numbers, int[] expectedResult)
+        {
+            Arrays test = new Arrays();
+            int[] actual = test.KeepLast(numbers);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase(new[] { 2, 2, 3 }, true)]
+        [TestCase(new[] { 3, 4, 5, 3 }, true)]
+        [TestCase(new[] { 2, 3, 2, 2 }, false)]
+        public void Double23_Test(int[] numbers, bool expectedResult)
+        {
+            Arrays test = new Arrays();
+            bool actual = test.Double23(numbers);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase(new[] { 1, 2, 3 }, new[] { 1, 2, 0 })]
+        [TestCase(new[] { 2, 3, 5 }, new[] { 2, 0, 5 })]
+        [TestCase(new[] { 1, 2, 1 }, new[] { 1, 2, 1 })]
+        public void Fix23_Test(int[] numbers, int[] expectedResult)
+        {
+            Arrays test = new Arrays();
+            int[] actual = test.Fix23(numbers);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase(new[] { 1, 3, 4, 5 }, true)]
+        [TestCase(new[] { 2, 1, 3, 4, 5 }, true)]
+        [TestCase(new[] { 1, 1, 1 }, false)]
+        public void Unlucky1_Test(int[] numbers, bool expectedResult)
+        {
+            Arrays test = new Arrays();
+            bool actual = test.Unlucky1(numbers);
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestCase(new[] {4, 5}, new[] {1, 2, 3}, new[] {4, 5})]
+        [TestCase(new[] {4}, new[] {1, 2, 3}, new[] {4, 1})]
+        [TestCase(new[] {3,4,5}, new[] {1}, new[] { 3,4})]
+       [TestCase(new int []{}, new[] {1, 2}, new[] {1, 2})]
+        public void Make2_Test(int[] a, int[] b, int[] expectedResult)
+        {
+            Arrays test = new Arrays();
+            int[] actual = test.Make2(a, b);
+            Assert.AreEqual(expectedResult, actual);
+        }
     }
 }
--- Warmups/Warmups/Arrays.cs
+++ Warmups/Warmups/Arrays.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace Warmups
 {
@@ -40,7 +44,7 @@ namespace Warmups
         }
 
         /// <summary>
-        /// Exercise 02 MakePi
+        /// Exercise 03 MakePi
         /// </summary>
         /// <param name="n"></param>
         /// <returns></returns>
@@ -59,15 +63,205 @@ namespace Warmups
             return false;
         }
 
-        //public int Sum(int[] numbers)
-        //{
-        //    var sum = 0;
-        //    for (int i = 0; i < numbers.Length; i++)
-        //    {
-        //        sum = numbers[i];
-        //    }
-        //    return;
-        //}
+        /// <summary>
+        /// Exercise 05 Sum
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        public int Sum(int[] numbers)
+        {
+            int total = 0;
+            foreach(int number in numbers)
+            {
+              total += number;
+            }
+            return total;
+        }
+
+        /// <summary>
+        /// Exercise 06 RotateLeft
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        public int[] RotateLeft(int[] numbers)
+        {
+            int i = numbers [0];
+            for (int j = 0 ; j < numbers.Length-1; j++)
+            {
+                numbers[j] = numbers[j + 1];
+            }
+            numbers[numbers.Length - 1] = i;
+            return numbers;
+
+        }
+
+        /// <summary>
+        /// Exercise 07 Reverse
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        public int[] Reverse(int[] numbers)
+        {
+            int[] arr = new int[3];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                arr[i] = numbers [numbers.Length - 1 - i];
+            }
+            return arr;
+        }
+
+        /// <summary>
+        /// Exercise 08 HigherWins
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        public int[] HigherWins(int[] numbers)
+        {
+            int greaterValue;
+            if (numbers[0] > numbers[numbers.Length - 1])
+            {
+                greaterValue = numbers[0];
+            }
+            else
+            {
+                greaterValue = numbers[numbers.Length-1];
+            }
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = greaterValue;
+            }
+            return numbers;
+        }
+
+        /// <summary>
+        /// Exercise 09 GetMiddle
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int[] GetMiddle(int[] a, int[] b)
+        {
+            int[] newArray = new int[2];
+
+            newArray[0] = a[1];
+            newArray[1] = b[1];
+            return newArray;
+        }
+
+        /// <summary>
+        /// Exercise 10 HasEven
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        public bool HasEven(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i]%2 == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Exercise 11 KeepLast
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        public int[] KeepLast(int[] numbers)
+        {
+            int[] newArray = new int[numbers.Length*2];
+            newArray[newArray.Length - 1] = numbers[numbers.Length - 1];
+            return newArray;
+
+        }
+
+
+        public bool Double23(int[] numbers)
+        {
+            int count2 = 0;
+            int count3 = 0;
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    if (numbers[i] == 2)
+                    {
+                        count2++;
+                    }
+                    else if (numbers[i] == 3)
+                    {
+                        count3++;
+                    }
+                }
+
+            if (count2 == 2 || count3 == 2)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Exercise 13 Fix23
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        public int[] Fix23(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                if (numbers[i] == 2 && numbers[i + 1] == 3)
+                {
+                    numbers[i + 1] = 0;
+                }
+            }
+            return numbers;
+        }
+
+        /// <summary>
+        /// Exercise 14 Unlucky1
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        public bool Unlucky1(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                if (numbers[i] == 1 && numbers[i + 1] == 3)
+                {
+                    if (i == 0 || i == 1 || i == numbers.Length - 2)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Exercise 15 Make2
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int[] Make2(int[] a, int[] b)
+        {
+            int[] newArray = new int[2];
+            if (a.Length >= 2)
+            {
+                newArray[0] = a[0];
+                newArray[1] = a[1];
+                return newArray;
+            }
+            else if (a.Length < 2)
+            {
+                newArray[0] = a[0];
+                newArray[1] = b[0];
+                return newArray;
+            }
+            return newArray;
+        }
 
     }
 }
